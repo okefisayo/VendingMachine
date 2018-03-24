2aceffc8fa91ffa02d5d35f718004a23a89cb386
Oluwafisayo <okefisayo98@gmail.com>
2018-03-23 21:50:23 -0400

    added test class

---
+++ ButtonViewer1.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ButtonViewer1{
+	public static void main(String[]  args){
+		JFrame frame = new VendingMachine();
+		frame.setTitle("vending Machine");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
